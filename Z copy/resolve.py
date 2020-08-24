3bb4a300999a0ee22c79fed079f0905563d897d8
Sho Taguchi <taguchi.sho@mail.canon>
2020-08-24 12:53:04 +0900

    aa

--- Z copy/resolve.py
+++ Z copy/resolve.py
@@ -2,8 +2,13 @@ def resolve():
     '''
     code here
     '''
-    A, B, C, K = 
+    A, B, C, K = [int(item) for item in input().split()]
 
+    if K % 2 == 0:
+        res = A- B
+    else:
+        res = B -A
 
+    print(res)
 if __name__ == "__main__":
     resolve()
